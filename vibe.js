4d9d9c8bb1672347b972ceb1509927640677bee5
nicoleleenk <nicole30nov@gmail.com>
2026-01-09 18:00:56 +0800

    vibe.js

--- vibe.js
+++ vibe.js
@@ -1,1 +1,18 @@
+const rain = document.getElementById("rainSound");
+const thunder = document.getElementById("thunderSound");
+
+document.body.addEventListener("click", () => {
+  if (rain.paused) {
+    rain.volume = 0.35;
+    rain.play();
+  }
+}, { once: true });
+
+// Occasional thunder
+setInterval(() => {
+  if (Math.random() < 0.2) {
+    thunder.volume = 0.4;
+    thunder.play();
+  }
+}, 25000);
 
